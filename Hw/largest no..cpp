21c080c613450057d89260c8db96d8322f57674a
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-08-07 15:55:59 +0530

    Add files via upload

---
+++ Hw/largest no..cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+    
+    cout << "Enter the first number: ";
+    cin >> num1;
+    
+    cout << "Enter the second number: ";
+    cin >> num2;
+    
+    cout << "Enter the third number: ";
+    cin >> num3;
+    
+    int largest = num1; 
+    
+    if (num2 > largest) {
+        largest = num2;
+    }
+    
+    if (num3 > largest) {
+        largest = num3;
+    }
+    
+    cout << "The largest number is: " << largest << endl;
+    
+    return 0;
+}
+
