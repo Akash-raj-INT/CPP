04bdf221bb8e65d2726c9cfe5467987300cbadea
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-08-08 14:42:03 +0530

    Add files via upload

---
+++ Array/enum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    enum days{sun, mon, tue, wed, thru, fri, sat};
+     days d1, d2;
+     d1 = sun;
+     d2 = wed;
+
+     cout<<d1<<endl<<d2<<endl;
+}
