04dd3c95ad0aa9be4f8650194f62f94c72627473
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-08-10 22:20:27 +0530

    Add files via upload

---
+++ Array/enum2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    enum days{jan, feb, mar, april, may, jun, july};
+     days M1, M2, M3;
+     M1 = jan;
+     M2 = april;
+     M3 = july;
+
+     cout<<M1<<endl<<M2<<endl<<M3<<endl;
+     return 0;
+}
