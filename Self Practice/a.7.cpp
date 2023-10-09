018cc3ad3c70ea6c15804278ac91da5c5f300737
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-10-10 00:18:09 +0530

    Add files via upload

---
+++ Self Practice/a.7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin>>a;
+    cin>>b;
+
+    cout<<"Value of a is: "<<a<<endl;
+    cout<<"Value of b is: "<<b<<endl;
+}
