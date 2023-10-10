5990ec9e2aa11461c05cad41b27628bdd2ddd379
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-10-11 00:13:16 +0530

    Add files via upload

---
+++ Self Practice/b3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 25;
+  int votingAge = 18;
+  
+  cout << (myAge >= votingAge);  // returns 1 (true), meaning 25 year olds are allowed to vote!
+  return 0;
+}
